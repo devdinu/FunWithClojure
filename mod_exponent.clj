0e505bd849c0ef051a0729cc3a4889f01bcb98e2
Dinesh Kumar <dineshkb@thoughtworks.com>
2015-07-11 00:58:07 +0530

    returning proper value modpow

--- mod_exponent.clj
+++ mod_exponent.clj
@@ -3,42 +3,30 @@
 (defn isodd? [n] (= (mod n 2) 1))
 
 (defn getresult [base p result]
-  (if (isodd? (>> p)) (* result base) result)
+  (if (isodd? p) (* result base) result)
 )
 
 (def squaremod (fn [n m] (mod (* n n) m) ))
 
 (defn getval [base exp m result]
-  (if (zero? exp) 1
+  (if (zero? exp) result
       (getval (squaremod base m)  (>> exp) m (getresult base exp result) )
   )
-  (println base exp m result "..... at getval")
 )
 
 (defn sumr [x]
-  (println "sum " x)
-  (if (<= x 0) 0
-      (+ 0 (sumr (- x 1)))
-      )
+   (if (zero? x) 0 (+ x (sumr (dec x))))
 )
 
 (defn modpow [n p m]
-	(cond
-		(= n 0) 0
-		:else (print (getval n p m 1) "is ret") 
-	)
+  (cond (zero? n) 0 :else (getval n p m 1))
 )
 
-(defn exponentiation_recursive [^long x p]
-  (println x p)
-  (cond
-		(= p 0) 1
-	       :else (exponentiation_recursive (* x x) (- p 1))
-  )
+(defn exponentiation_recursive [x p]
+  (if (zero? p) 1 (* x (exponentiation_recursive x (dec p))) )
 )
-
 
 (println (sumr 5) )
 (println (modpow 2 1 10000))
-(println (modpow 2 8 10000)) 
-;;; (println (exponentiation_recursive 2 8))
+(println (modpow 2 8 10000))
+(println (exponentiation_recursive 2 8))
