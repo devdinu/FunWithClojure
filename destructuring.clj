ef74ddc93b7a5083a0375896d9f9207f499292da
Dinesh Kumar <dineshkb@thoughtworks.com>
2015-07-27 10:41:23 +0530

    Asserting With Tests

--- destructuring.clj
+++ destructuring.clj
@@ -1,5 +1,19 @@
+(ns desttructuring
+  (:use clojure.test))
+
 (defn is-same-line? [cord1 cord2]
   (let [[x1 y1] cord1
         [x2 y2] cord2]
-    (println x1 x2 y1 cord1)
     (= x1 x2)))
+
+
+
+(def cord1 [1 10])
+(def cord2 [1 20])
+(def cord3 [10 10])
+
+
+
+
+(is (is-same-line? cord1 cord2))
+(is (not (is-same-line? cord1 cord3)))
